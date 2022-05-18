3e1f331f880ef774fdb8498e1e760646a7ea0606
trevor dewindt <trevor.dewindt@gmail.com>
2022-05-17 20:11:48 -0500

    commit tree 6465d42e73ee8d39a6de78a60a6f74d48bb5ed0d

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+
+function appendCat(name) {
+    const moreCatsBack = [...cats, name];
+    return moreCatsBack;
+}
+
+
+function prependCat(name) {
+    const moreCatsFront = [name, ...cats];
+    return moreCatsFront;
+}
+
+
+function removeLastCat(name) {
+    return cats.slice(0,-1);
+}
+
+
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
